5131e9ef240b9fe48ec7dd206ae68973cc4c9e35
Luandantas-06 <luandantas760@gmail.com>
2025-07-19 14:11:29 -0300

    Update ProdutoPerecivel.java

--- loja/model/produto/ProdutoPerecivel.java
+++ loja/model/produto/ProdutoPerecivel.java
@@ -4,13 +4,25 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 
-public class ProdutoPerecivel extends ProdutoFisico{
+public class ProdutoPerecivel extends Produto{
     private LocalDate dataValidade;
+    private double pesoGramas;
+    private int estoque;
 
     
-    public ProdutoPerecivel(String codigo, String nome, BigDecimal precoBase, int pesoGramas, LocalDate dataValidade, int estoque) {
-        super(codigo, nome, precoBase, pesoGramas, estoque);
+    public ProdutoPerecivel(String codigo, String nome, BigDecimal precoBase, double pesoGramas, LocalDate dataValidade, int estoque) {
+        super(codigo, nome, precoBase);
         this.dataValidade = dataValidade;
+        this.pesoGramas = pesoGramas;
+        this.estoque = estoque;
+    }
+
+    public double getPesoGramas() {
+        return pesoGramas;
+    }
+
+    public void setPesoGramas(double pesoGramas) {
+        this.pesoGramas = pesoGramas;
     }
 
     public LocalDate getDataValidade() {
@@ -20,7 +32,14 @@ public class ProdutoPerecivel extends ProdutoFisico{
     public void setDataValidade(LocalDate dataValidade) {
         this.dataValidade = dataValidade;
     }
-    
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
     public boolean estaVencido() {
         return dataValidade.isBefore(LocalDate.now());
     }
