e7ae7126f5f24f6adb5d3925532d0c07f1c1aeed
Thayná Gonçalves <thaynalgoncalves@academico.ufs.br>
2025-07-23 10:21:01 -0300

    Create ProdutoInterface.java

---
+++ loja/model/produto/ProdutoInterface.java
@@ -0,0 +1,11 @@
+public interface ProdutoInterface {
+    String getIdentificador();
+    void setIdentificador();
+    String getNome();
+    void setNome();
+    String getEndereco();
+    void setEndereco();
+    String getTelefone();
+    void setTelefone();
+    void exibirCliente();
+}
